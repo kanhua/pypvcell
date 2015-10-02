c4c4f38f2d30bf102c16b22dda5496c2dda5c8e1
kanhua <kanhwa@gmail.com>
2015-10-02 11:16:01 +0900

    Update calc_3j_si_eta() and add 3J plots in plot_profiles.py

--- iii_v_si.py
+++ iii_v_si.py
@@ -2,14 +2,13 @@ __author__ = 'kanhua'
 
 import numpy as np
 from ivsolver import calculate_j01_from_qe, gen_rec_iv, calculate_j01, calculate_j02_from_rad_eff
-from detail_balanced_MJ import calc_mj_eta,rad_to_voc,extract_voc
+from detail_balanced_MJ import calc_mj_eta, rad_to_voc, extract_voc
 from photocurrent import gen_qe_from_abs, gen_square_qe, calc_jsc
 from spectrum_base import spectrum_base
 import matplotlib.pyplot as plt
 from fom import voc
 from illumination import illumination, bp_filter
 from scipy.interpolate import interp2d
-
 
 
 def calc_2j_si_eta(si_layer_t, si_rad_eta, top_cell_bg, top_cell_qe=1, top_cell_rad_eta=1):
@@ -42,7 +41,7 @@ def calc_2j_si_eta(si_layer_t, si_rad_eta, top_cell_bg, top_cell_qe=1, top_cell_
 
     si_voc = extract_voc(v, i, qe)
 
-    top_voc=rad_to_voc(top_cell_rad_eta,gen_square_qe(top_cell_bg,top_cell_qe))
+    top_voc = rad_to_voc(top_cell_rad_eta, gen_square_qe(top_cell_bg, top_cell_qe))
 
     eta = calc_mj_eta([top_cell_bg, 1.1], [top_cell_qe, 1], [top_cell_rad_eta, si_rad_eta], 300, replace_iv=(1, (v, i)),
                       replace_qe=(1, qe))
@@ -50,19 +49,17 @@ def calc_2j_si_eta(si_layer_t, si_rad_eta, top_cell_bg, top_cell_qe=1, top_cell_
     return eta, si_voc, top_voc
 
 
-def calc_3j_si_eta(top_cell_eta,mid_cell_eta, concentration,top_band_gap=1.87, mid_band_gap=1.42) :
-
-    si_bg=1.12
+def calc_3j_si_eta(top_cell_eta, mid_cell_eta, concentration,
+                   top_band_gap=1.87, top_cell_qe=1, mid_band_gap=1.42, mid_cell_qe=1):
+    si_bg = 1.12
     cell_temperature = 300
-    subcell_eg = np.array([top_band_gap, mid_band_gap,si_bg])
-    subcell_qe = np.array([1, 1, 1])
-    subcell_rad_eff = np.array([top_cell_eta, top_cell_eta,mid_cell_eta])
+    subcell_eg = np.array([top_band_gap, mid_band_gap, si_bg])
+    subcell_qe = np.array([top_cell_qe, mid_cell_qe, 1])
+    subcell_rad_eff = np.array([top_cell_eta, top_cell_eta, mid_cell_eta])
 
+    top_voc = rad_to_voc(top_cell_eta, gen_square_qe(top_band_gap, subcell_qe[0]))
+    mid_voc = rad_to_voc(top_cell_eta, gen_square_qe(mid_band_gap, subcell_qe[1]))
+    bot_voc = rad_to_voc(mid_cell_eta, gen_square_qe(si_bg, subcell_qe[2]))
 
-    top_voc=rad_to_voc(top_cell_eta,gen_square_qe(top_band_gap,subcell_qe[0]))
-    mid_voc=rad_to_voc(top_cell_eta,gen_square_qe(mid_band_gap,subcell_qe[1]))
-    bot_voc=rad_to_voc(mid_cell_eta,gen_square_qe(si_bg,subcell_qe[2]))
-
-
-    return calc_mj_eta(subcell_eg,subcell_qe,subcell_rad_eff,cell_temperature,concentration=concentration), \
-           top_voc,mid_voc,bot_voc
+    return calc_mj_eta(subcell_eg, subcell_qe, subcell_rad_eff, cell_temperature, concentration=concentration), \
+           top_voc, mid_voc, bot_voc
