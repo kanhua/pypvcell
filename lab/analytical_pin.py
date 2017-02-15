db0e5f528b7de7bf272545e0559af294cd99ec46
kanhua <kanhwa@gmail.com>
2017-02-15 17:36:33 +0900

    Cut out core Jn and Jp calculation in Markus's code

--- lab/analytical_pin.py
+++ lab/analytical_pin.py
@@ -170,82 +170,11 @@ def calculate_junction_sr(junc, energies, bs, bsInitial, V, printParameters=Fals
         s_bottom, s_top = sp, sn
         d_bottom, d_top = dp, dn
 
-    harg_bottom = (x_bottom - w_bottom) / l_bottom
-    cosh_harg_bottom = cosh(harg_bottom)
-    sinh_harg_bottom = sinh(harg_bottom)
-    lsod_top = (l_top * s_top) / d_top
-    # n0 = niSquared/Na
-
-    harg_top = (x_top - w_top) / l_top
-    sinh_harg_top = sinh(harg_top)
-    cosh_harg_top = cosh(harg_top)
-    lsod_bottom = (l_bottom * s_bottom) / d_bottom
-    # p0 = niSquared/Nd
-    # print ("lsod_bottom",lsod_bottom)
-    # print ("lspd_top",lspd_top)
-
-    # j_top= (q*bs_incident_on_top*alpha*l_bottom)/(alpha**2*l_bottom**2-1)* \
-    #     ((lsod_bottom+ alpha*l_bottom - exp(-alpha*(x_top-w_top)) * (lsod_bottom*cosh_harg_top+sinh_harg_top))      /(lsod_bottom*sinh_harg_top+cosh_harg_top)
-    #     - alpha*l_bottom* exp(-alpha*(x_top-w_top))
-    # )
-
-    # j_bottom= q*(bs_incident_on_bottom*alpha*l_top/(alpha**2*l_top**2-1) \
-    #     *(l_top*alpha -
-    #     ((lspd_top*(-exp(-alpha*(x_bottom-w_bottom))+cosh_harg_bottom))+sinh_harg_bottom + (l_top*alpha)*exp(-alpha*(x_bottom-w_bottom)))     /(cosh_harg_bottom +lspd_top*sinh_harg_bottom))
-    # )
-
-    j_top = (q * bs_incident_on_top * alphaTop * l_top) / (alphaTop ** 2 * l_top ** 2 - 1) * \
-            ((lsod_top + alphaTop * l_top - exp(-alphaTop * (x_top - w_top)) * (
-            lsod_top * cosh_harg_top + sinh_harg_top)) / (lsod_top * sinh_harg_top + cosh_harg_top)
-             - alphaTop * l_top * exp(-alphaTop * (x_top - w_top))
-             )
-
-    j_bottom = q * (bs_incident_on_bottom * alphaBottom * l_bottom / (alphaBottom ** 2 * l_bottom ** 2 - 1) \
-                    * (l_bottom * alphaBottom -
-                       ((lsod_bottom * (
-                       -exp(-alphaBottom * (x_bottom - w_bottom)) + cosh_harg_bottom)) + sinh_harg_bottom + (
-                        l_bottom * alphaBottom) * exp(-alphaBottom * (x_bottom - w_bottom))) / (
-                       cosh_harg_bottom + lsod_bottom * sinh_harg_bottom))
-                    )
-
-    # jgen=q*bs_incident_on_depleted*(1 - exp(-alpha*(depleted_width)))    # jgen. Jenny, p. 159
-    jgen = q * bs_incident_on_depleted * (1 - exp(-alphaI * xi - alphaN * wn - alphaP * wp))  # jgen. Jenny, p. 159
-
-    # hereby I define the subscripts to refer to the layer in which the current is generated:
-    if pn_or_np == "pn":
-        jn, jp = j_bottom, j_top
-    else:
-        jp, jn = j_bottom, j_top
-
-    # Jrec. Jenny, p.159. Note capital J. This does not need integrating over energies
-    lifetime_n = ln ** 2 / dn  # these might not be the right lifetimes. Check ###
-    lifetime_p = lp ** 2 / dp  # Jenny p163
-
-    Jrec = q * ni * (wn + wp + xi) / sqrt(lifetime_n * lifetime_p) * sinh(q * V / (2 * kbT)) / (
-    q * (Vbi - V) / kbT) * pi
-
-    # jgen = q* bs*(1 - exp(-depleted_width*alpha))*exp(-(xn-wn)*alpha); print ("Ned hack")
-    nDepletionCharge = wn * Nd * q
-    pDepletionCharge = wp * Na * q
-
-    Vbi2 = (0.5 * (wn + wp) + xi) * pDepletionCharge / (es)
-
-    good_indeces = isfinite(jn) * isfinite(jp) * isfinite(jgen)
-
-    jn[jn < 0] = 0
-    jp[jp < 0] = 0
-    jgen[jgen < 0] = 0
-
-    energies = energies[good_indeces]
-    jn = jn[good_indeces]
-    jp = jp[good_indeces]
-    jgen = jgen[good_indeces]
-
-    bsInitial = bsInitial[good_indeces]
-
-    Jn = trapz(y=jn, x=energies)
-    Jp = trapz(y=jp, x=energies)
-    Jgen = trapz(y=jgen, x=energies)
+    Jgen, Jn, Jp, Jrec, bsInitial, energies, jgen, jn, jp = calc_jnp(V, Vbi, alphaBottom, alphaI, alphaTop, bsInitial,
+                                                                     bs_incident_on_bottom, bs_incident_on_depleted,
+                                                                     bs_incident_on_top, d_bottom, d_top, energies, T,
+                                                                     l_bottom, l_top, ni, pn_or_np, s_bottom, s_top,
+                                                                     w_bottom, w_top, x_bottom, x_top, xi)
 
     return {
         "qe_n": jn / q / bsInitial,
@@ -260,4 +189,110 @@ def calculate_junction_sr(junc, energies, bs, bsInitial, V, printParameters=Fals
         "e": energies,
         "Temporary locals dictionary for radiative efficiency": locals()
     }
+
+
+def calc_jnp(V, Vbi, alphaBottom, alphaI, alphaTop, bsInitial, bs_incident_on_bottom, bs_incident_on_depleted,
+             bs_incident_on_top, d_bottom, d_top, energies, T, l_bottom, l_top, ni, pn_or_np, s_bottom, s_top, w_bottom,
+             w_top, x_bottom, x_top, xi):
+
+    """
+    Calulate electron and hole current densities using analytical equations in Jenny's book
+
+    :param V: voltage
+    :param Vbi: built-in voltage
+    :param alphaBottom: absorption coefficient of the bottom layer
+    :param alphaI: absorption coefficient of the intrinsic layer
+    :param alphaTop: absorption coefficient of the top layer
+    :param bsInitial: set to None if it is identical to bs_incident_on_top
+    :param bs_incident_on_bottom:
+    :param bs_incident_on_depleted:
+    :param bs_incident_on_top: incident photon plux density. This can be unity because it will be normalized when calculating QE
+    :param d_bottom: diffusion coefficient of bottom layer
+    :param d_top: diffusion coefficient of top layer
+    :param energies: the range of energy to be calculated
+    :param T: temperature in Kelvins
+    :param l_bottom: diffusion length of the bottom layer
+    :param l_top: diffusion length of the top layer
+    :param ni: intrinsic doping density
+    :param pn_or_np:
+    :param s_bottom: surface recombination velocity of the bottom layer
+    :param s_top: surface recombination velocity of the top layer
+    :param w_bottom: depletion width of the bottom layer
+    :param w_top: depletion width of the top layer
+    :param x_bottom: the thickness of the bottom layer
+    :param x_top: the thickness of the top layer
+    :param xi: the thickness of the intrinsic layer
+    :return:
+    """
+    kbT=kb*T
+
+    harg_bottom = (x_bottom - w_bottom) / l_bottom
+    cosh_harg_bottom = cosh(harg_bottom)
+    sinh_harg_bottom = sinh(harg_bottom)
+    lsod_top = (l_top * s_top) / d_top
+    # n0 = niSquared/Na
+    harg_top = (x_top - w_top) / l_top
+    sinh_harg_top = sinh(harg_top)
+    cosh_harg_top = cosh(harg_top)
+    lsod_bottom = (l_bottom * s_bottom) / d_bottom
+    # p0 = niSquared/Nd
+    # print ("lsod_bottom",lsod_bottom)
+    # print ("lspd_top",lspd_top)
+    # j_top= (q*bs_incident_on_top*alpha*l_bottom)/(alpha**2*l_bottom**2-1)* \
+    #     ((lsod_bottom+ alpha*l_bottom - exp(-alpha*(x_top-w_top)) * (lsod_bottom*cosh_harg_top+sinh_harg_top))      /(lsod_bottom*sinh_harg_top+cosh_harg_top)
+    #     - alpha*l_bottom* exp(-alpha*(x_top-w_top))
+    # )
+    # j_bottom= q*(bs_incident_on_bottom*alpha*l_top/(alpha**2*l_top**2-1) \
+    #     *(l_top*alpha -
+    #     ((lspd_top*(-exp(-alpha*(x_bottom-w_bottom))+cosh_harg_bottom))+sinh_harg_bottom + (l_top*alpha)*exp(-alpha*(x_bottom-w_bottom)))     /(cosh_harg_bottom +lspd_top*sinh_harg_bottom))
+    # )
+    j_top = (q * bs_incident_on_top * alphaTop * l_top) / (alphaTop ** 2 * l_top ** 2 - 1) * \
+            ((lsod_top + alphaTop * l_top - exp(-alphaTop * (x_top - w_top)) * (
+                lsod_top * cosh_harg_top + sinh_harg_top)) / (lsod_top * sinh_harg_top + cosh_harg_top)
+             - alphaTop * l_top * exp(-alphaTop * (x_top - w_top))
+             )
+    j_bottom = q * (bs_incident_on_bottom * alphaBottom * l_bottom / (alphaBottom ** 2 * l_bottom ** 2 - 1) \
+                    * (l_bottom * alphaBottom -
+                       ((lsod_bottom * (
+                           -exp(-alphaBottom * (x_bottom - w_bottom)) + cosh_harg_bottom)) + sinh_harg_bottom + (
+                            l_bottom * alphaBottom) * exp(-alphaBottom * (x_bottom - w_bottom))) / (
+                           cosh_harg_bottom + lsod_bottom * sinh_harg_bottom))
+                    )
+    # jgen=q*bs_incident_on_depleted*(1 - exp(-alpha*(depleted_width)))    # jgen. Jenny, p. 159
+
+    jgen = q * bs_incident_on_depleted * (1 - exp(-alphaI * xi - alphaTop * w_top - alphaBottom * w_bottom))  # jgen. Jenny, p. 159
+    # hereby I define the subscripts to refer to the layer in which the current is generated:
+    if pn_or_np == "pn":
+        jn, jp = j_bottom, j_top
+    else:
+        jp, jn = j_bottom, j_top
+
+    # Jrec. Jenny, p.159. Note capital J. This does not need integrating over energies
+    lifetime_top = l_top ** 2 / d_top  # these might not be the right lifetimes. Check ###
+    lifetime_bottom = l_bottom ** 2 / d_bottom  # Jenny p163
+    Jrec = q * ni * (w_top + w_bottom + xi) / sqrt(lifetime_top * lifetime_bottom) * sinh(q * V / (2 * kbT)) / (
+        q * (Vbi - V) / kbT) * pi
+    # jgen = q* bs*(1 - exp(-depleted_width*alpha))*exp(-(xn-wn)*alpha); print ("Ned hack")
+    #nDepletionCharge = wn * Nd * q
+    #pDepletionCharge = wp * Na * q
+    #Vbi2 = (0.5 * (wn + wp) + xi) * pDepletionCharge / (es)
+
+
+
+    # Clean the calcualted results
+    good_indeces = isfinite(jn) * isfinite(jp) * isfinite(jgen)
+    jn[jn < 0] = 0
+    jp[jp < 0] = 0
+    jgen[jgen < 0] = 0
+    energies = energies[good_indeces]
+    jn = jn[good_indeces]
+    jp = jp[good_indeces]
+    jgen = jgen[good_indeces]
+    bsInitial = bsInitial[good_indeces]
+    Jn = trapz(y=jn, x=energies)
+    Jp = trapz(y=jp, x=energies)
+    Jgen = trapz(y=jgen, x=energies)
+
+
+    return Jgen, Jn, Jp, Jrec, bsInitial, energies, jgen, jn, jp
 
